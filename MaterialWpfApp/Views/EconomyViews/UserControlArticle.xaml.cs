a4101bf6b80f4cf1b9b754b927d67edb606e50cf
ewgen98 <ewgen9826@list.ru>
2018-06-15 10:17:40 +0300

    Add c

--- MaterialWpfApp/Views/EconomyViews/UserControlArticle.xaml.cs
+++ MaterialWpfApp/Views/EconomyViews/UserControlArticle.xaml.cs
@@ -1,20 +1,7 @@
-﻿using MaterialDesignThemes.Wpf.Transitions;
-using MaterialWpfApp.ViewModels.Economy;
+﻿using MaterialWpfApp.ViewModels.Economy;
 using MaterialWpfApp.ViewModels.Helper;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MaterialWpfApp.Views.EconomyViews
 {
@@ -27,7 +14,9 @@ namespace MaterialWpfApp.Views.EconomyViews
         {
             InitializeComponent();
             this.DataContext = new ArticleViewModel(date);
-            TrainsitionigContentSlide.OpeningEffect = new TransitionEffect(transition, new TimeSpan(0, 0, 0, 0, 800));
+
+
+            TrainsitionigContentSlide.OpeningEffect = new TransitionEffect(transition, new TimeSpan(0, 0, 0, 0, 800))"";
 
 
         }
